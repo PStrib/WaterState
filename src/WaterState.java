de03adfed61ebad5afa0b3cdd74444e08a002eca
PStrib <peter.strib@gmail.com>
2022-10-10 22:50:15 +0100

    Makes the program function up to spec

--- src/WaterState.java
+++ src/WaterState.java
@@ -1,5 +1,40 @@
+import java.util.Scanner;
+
+/**
+ * Program to find the state of water at any given temperature and altitude
+ * @author Peter Stribblehill
+ * @version 10/10/2022
+ */
 public class WaterState {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args){
+        Scanner scanner=new Scanner(System.in);
+        final int seaLevelBoilingPoint=100;
+
+        System.out.println("What is the temperature? (to the nearest degree c)");
+        int temperature=scanner.nextInt();
+        //if the temperature is below 0 then the altitude doesn't matter
+        if (temperature<0){
+            System.out.println("Water is solid at this temperature");
+            return;
+        }
+
+        System.out.println("What is the altitude? (to the nearest metre above sea level)");
+        int altitude=scanner.nextInt();
+        while(altitude<0){
+            System.out.println("Sorry, please enter a height above sea level");
+            altitude=scanner.nextInt();
+        }
+
+        int degreesAboveSeaLevel=altitude/300;
+        int newBoilingPoint=seaLevelBoilingPoint+degreesAboveSeaLevel;
+
+        String state;
+        //If the temperature is over the altitude adjusted boiling point, the state is set to 'gaseous'
+        if(temperature>=newBoilingPoint){state="gaseous";}
+        //If not then the state is set to 'liquid'
+        else{state="liquid";}
+
+        System.out.println("The boiling point of water at this height is "+newBoilingPoint+" degrees c");
+        System.out.println("Water is "+state+" at these conditions");
     }
 }
