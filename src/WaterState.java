230c6b58417a7734b2ca46882652c9a747bbd2e1
PStrib <peter.strib@gmail.com>
2022-10-11 12:27:01 +0100

    Implements constants rather than magic numbers.

--- src/WaterState.java
+++ src/WaterState.java
@@ -9,23 +9,26 @@ public class WaterState {
     public static void main(String[] args){
         Scanner scanner=new Scanner(System.in);
         final int seaLevelBoilingPoint=100;
+        final int seaLevel=0;
+        final int degreeIncrement=300;
+        final int freezingPoint=0;
 
         System.out.println("What is the temperature? (to the nearest degree c)");
         int temperature=scanner.nextInt();
         //if the temperature is below 0 then the altitude doesn't matter
-        if (temperature<0){
+        if (temperature<freezingPoint){
             System.out.println("Water is solid at this temperature");
             return;
         }
 
         System.out.println("What is the altitude? (to the nearest metre above sea level)");
         int altitude=scanner.nextInt();
-        while(altitude<0){
+        while(altitude<seaLevel){
             System.out.println("Sorry, please enter a height above sea level");
             altitude=scanner.nextInt();
         }
 
-        int degreesAboveSeaLevel=altitude/300;
+        int degreesAboveSeaLevel=altitude/degreeIncrement;
         int newBoilingPoint=seaLevelBoilingPoint-degreesAboveSeaLevel;
 
         String state;
